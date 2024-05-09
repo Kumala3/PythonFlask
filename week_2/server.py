775ba5b0b11600e281ddbb666348008c1de7fcc1
Kostek <ksytnik73kwer@gmail.com>
2024-05-09 15:51:14 +0200

    Add fake_db.py and update server.py

--- week_2/server.py
+++ week_2/server.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, make_response, Request
+from fake_db import data
 
 app = Flask(__name__)
 
-@app.route("/")
-def general():
-    return {"message": "Hello, World!"}
+@app.route("/no_content", methods=["GET"])
+def no_content():
+    return ({"message": "No content found"}, 204)
+
+
+@app.route("/exp", methods=["GET"])
+def index_explicit():
+    res = make_response({"message": "Hello, World!"})
+    res.status_code = 200
+    return res
+
+
+@app.route("/", methods=["GET"])
+def general(request: Request):
+    first_name = request.query.get("first_name")
+    return {"message": f"Hello, {first_name}"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4112)
+    app.run(host="localhost", port=5000, debug=True)
